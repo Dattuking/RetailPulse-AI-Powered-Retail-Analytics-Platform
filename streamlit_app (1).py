42c3837afe45808b7c5a96274108e550c769ef5d
Venkata Datta Sai Krishna Kona <164836917+Dattuking@users.noreply.github.com>
2026-05-28 10:38:04 +0530

    Add files via upload

---
+++ streamlit_app (1).py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title="RetailPulse Dashboard", layout="wide")
+
+st.title("RetailPulse – AI Powered Retail Analytics Platform")
+
+st.write("""
+Welcome to RetailPulse.
+
+This dashboard provides:
+- EDA
+- Sales Analysis
+- Customer Segmentation
+- Churn Analysis
+- Inventory Insights
+""")
+
+st.success("Use the sidebar to navigate between pages.")
